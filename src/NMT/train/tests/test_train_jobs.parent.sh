9882c64f75877940fa8cb6e171523ac59c4a4566
Peter Bickel <pbickel@login03.rc.byu.edu>
2026-06-24 10:05:02 -0600

    added tests, modified .gitignore

---
+++ src/NMT/train/tests/test_train_jobs.parent.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --time=12:00:00   # walltime.  hours:minutes:seconds
+#SBATCH --mem=1024000M
+#SBATCH --gpus=0
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user thebrendanhatch@gmail.com
+#SBATCH --output /home/hatch5o6/CharLOTTE2.0/src/NMT/train/tests/%j_%x.out
+#SBATCH --job-name=test_train_jobs.parent.sh
+#SBATCH --qos=matrix
+
+python src/NMT/train/tests/test_train_jobs.py --tests reproduce_parent
